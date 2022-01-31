f0681d2c531e3dd33df1bf4fea8b61fc2f4994e8
shreyanshRP <shreyanshrp21@gmail.com>
2022-01-31 14:13:41 +0530

    Create Fahrenheit to Celcius.py
    Python program to convert Fahrenheit to Celsius.

---
+++ Fahrenheit to Celcius.py
@@ -0,0 +1,6 @@
+#Python Program to convert temperatur from Ferenheit to Celcius
+#github.com/shreyanshrp
+
+f = int(input("Enter temperature in Ferenheit: "))
+c = (f - 32)*5/9
+print(c , "celcius")
