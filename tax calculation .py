5004a376e64aca05ca46215922d5bd6e91577f18
shreyanshRP <shreyanshrp21@gmail.com>
2022-02-02 17:57:22 +0530

    Update tax calculation .py
    Added an option to customize %tax to be deducted in each category .

--- tax calculation .py
+++ tax calculation .py
@@ -1,15 +1,41 @@
 # A python program to accept the monthly income of an employee and display the income tax to be paid at the end of the year according to a specific criteria.
 # shreyanshrp
+
+print("""Tax slab is as follows:
+nil tax for annual income <=500k
+2% tax for annual income in range (500k 1M]
+4% tax for annual income >1M""")
+
+slab_change = input("Do you want to change? (y/n)")
+
+#slab settings:
+if slab_change == "y" :
+    #new slab
+    a=float(input("enter % tax for =<500k : "))
+    b=float(input("enter % tax for (500k  1M] : "))
+    a=float(input("enter % tax for >1M : "))
+    
+elif slab_change == "n" :
+    #default slab
+    a=0
+    b=2
+    c=4
+else:
+    print("Invalid input, write only 'y' or 'n' (without quotes)")
+
+#income input:
 incm_m= float(input("enter your monthly income:"))
 incm= incm_m * 12
+
+#Tax Calculation:
 if incm > 1000000 :
-    print("Your annual  income is: ", incm , " So,applicable tax % = 4%")
-    print("Tax to be paid= " , 0.04*incm)
+    print("Your annual  income is: ", incm , " So,applicable tax % =" , c , "%")
+    print("Tax to be paid= " , (c/100)*incm , "yearly")
 elif incm > 500000 and incm <= 1000000 :
-    print("Your annual  income is: ", incm , " So,applicable tax % = 2%")
-    print("Tax to be paid= " , 0.02*incm)
+    print("Your annual  income is: ", incm , " So,applicable tax % =", b , "%")
+    print("Tax to be paid= " , (b/100)*incm , "yearly")
 elif incm <= 500000 :
-    print("Your annual  income is: ", incm , " So,applicable tax % = 0%")
-    print("Tax to be paid= " , 0.00*incm)
+    print("Your annual  income is: ", incm , " So,applicable tax % =" , a , "%")
+    print("Tax to be paid= " , (a/100)*incm , "yearly")
 else :
     print("Invalid Input , Make sure you don't add comma in that!")
