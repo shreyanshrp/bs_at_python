f7f2142d3fd1bed6181cf55bbffe2d986dbe68a5
shreyanshRP <shreyanshrp21@gmail.com>
2022-02-03 23:52:35 +0530

    Update bonus marking.py
    Removed case sensitivity for 'T' and 'L' as input for subject type.

--- bonus marking.py
+++ bonus marking.py
@@ -15,37 +15,37 @@ type=input("Enter your class type , Theory(T) / Lab(L) : ")
 
 #main:
 if marks >= 80 :
-    if type=='T':
+    if type=='T' or type=='t':
         print("You will get 8% Bonus!")
         print("Your final marks are: " , marks*1.08)
-    elif type=='L':
+    elif type=='L' or type=='l':
         print("You will get 6% Bonus!")
         print("Your final marks are: " , marks*1.06)
     else:
         print("enter valid class type! , either T or L")
 elif marks >= 60 and marks<80 :
-    if type=='T':
+    if type=='T' or type=='t':
         print("You will get 6% Bonus!")
         print("Your final marks are: " , marks*1.06)
-    elif type=='L':
+    elif type=='L' or type=='l':
         print("You will get 4% Bonus!")
         print("Your final marks are: " , marks*1.04)
     else:
         print("enter valid class type! , either T or L")
 elif marks >= 40 and marks < 60 :
-    if type=='T':
+    if type=='T' or type=='t':
         print("You will get 4% Bonus!")
         print("Your final marks are: " , marks*1.04)
-    elif type=='L':
+    elif type=='L' or type=='l':
         print("You will get 2% Bonus!")
         print("Your final marks are: " , marks*1.02)
     else:
         print("enter valid class type! , either T or L")
 elif marks > 40 :
-    if type=='T':
+    if type=='T' or type=='t':
         print("You will get 0% Bonus!")
         print("Your final marks are: " , marks*1.00)
-    elif type=='L':
+    elif type=='L' or type=='l':
         print("You will get 0% Bonus!")
         print("Your final marks are: " , marks*1.00)
     else:
